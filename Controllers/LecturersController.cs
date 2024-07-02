8913a697c2f4325be50e2f83edae72fc2f24887b
pmghotekar <pmghotekar123@gmail.com>
2024-07-02 20:15:12 +0530

    Added Database and Scaffolded V&C

---
+++ Controllers/LecturersController.cs
@@ -0,0 +1,156 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SchoolManagementApp.MVC.Data;
+
+namespace SchoolManagementApp.MVC.Controllers
+{
+    public class LecturersController : Controller
+    {
+        private readonly SchoolManagementDbContext _context;
+
+        public LecturersController(SchoolManagementDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Lecturers
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Lectures.ToListAsync());
+        }
+
+        // GET: Lecturers/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var lecture = await _context.Lectures
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (lecture == null)
+            {
+                return NotFound();
+            }
+
+            return View(lecture);
+        }
+
+        // GET: Lecturers/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Lecturers/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,FirstName,LastName")] Lecture lecture)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(lecture);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(lecture);
+        }
+
+        // GET: Lecturers/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var lecture = await _context.Lectures.FindAsync(id);
+            if (lecture == null)
+            {
+                return NotFound();
+            }
+            return View(lecture);
+        }
+
+        // POST: Lecturers/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,FirstName,LastName")] Lecture lecture)
+        {
+            if (id != lecture.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(lecture);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!LectureExists(lecture.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(lecture);
+        }
+
+        // GET: Lecturers/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var lecture = await _context.Lectures
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (lecture == null)
+            {
+                return NotFound();
+            }
+
+            return View(lecture);
+        }
+
+        // POST: Lecturers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var lecture = await _context.Lectures.FindAsync(id);
+            if (lecture != null)
+            {
+                _context.Lectures.Remove(lecture);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool LectureExists(int id)
+        {
+            return _context.Lectures.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Data/SchoolManagementDbContext.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace SchoolManagementApp.MVC.Data;
+
+public partial class SchoolManagementDbContext : DbContext
+{
+    public SchoolManagementDbContext(DbContextOptions<SchoolManagementDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Course> Courses { get; set; }
+
+    public virtual DbSet<Lecture> Lectures { get; set; }
+
+    public virtual DbSet<Student> Students { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Course>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Courses__3214EC07110A2F9D");
+
+            entity.HasIndex(e => e.Code, "UQ__Courses__A25C5AA72F0AF6DB").IsUnique();
+
+            entity.Property(e => e.Code).HasMaxLength(50);
+            entity.Property(e => e.Name).HasMaxLength(50);
+        });
+
+        modelBuilder.Entity<Lecture>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Lectures__3214EC076F3AF35B");
+
+            entity.Property(e => e.FirstName).HasMaxLength(50);
+            entity.Property(e => e.LastName).HasMaxLength(50);
+        });
+
+        modelBuilder.Entity<Student>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Students__3214EC0705792788");
+
+            entity.Property(e => e.FirstName).HasMaxLength(50);
+            entity.Property(e => e.LastName).HasMaxLength(50);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
